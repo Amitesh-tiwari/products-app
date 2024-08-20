da4a9df034eb6976646fd46a9619935323ef9817
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-08-20 20:19:44 +0530

    neccessary changes done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
-const crypto = require('crypto');
+
 const jwt = require('jsonwebtoken');
 const app = express();
 const PORT = process.env.PORT || 3000;
